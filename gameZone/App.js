172cca2e495dc998e0abb7ef55c7eb7311f288e5
Eriamiatoe Efe <eriamiatoeefe11@gmail.com>
2023-12-19 09:37:28 +0100

    solved custom fonts bug

--- gameZone/App.js
+++ gameZone/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import Home from './screens/home';
+import {View} from 'react-native';
+
+
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    'Nunito-Regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    
+  });
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <Home />
+    <View onLayout={onLayoutRootView}>
+      <Home />
+    </View>
   );
 }
+
+
+
+
+
+
+
--- gameZone/screens/home.js
+++ gameZone/screens/home.js
@@ -4,13 +4,27 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.titleText}>Home Screen</Text>
     </View>
+    
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     padding: 24,
   },
+  titleText: {
+    fontFamily: 'Nunito-Bold',
+    fontSize: 18,
+  }
 });
+
+
+
+
+
+
+
